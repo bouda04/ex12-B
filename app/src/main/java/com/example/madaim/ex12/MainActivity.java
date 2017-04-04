2a293b32ad5f4703330b83fd16ff36cc9e2d249b
KaiOrland <kayorland@gmail.com>
2017-04-04 11:37:01 +0300

    srgs

--- app/src/main/java/com/example/madaim/ex12/MainActivity.java
+++ app/src/main/java/com/example/madaim/ex12/MainActivity.java
@@ -2,42 +2,57 @@ package com.example.madaim.ex12;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Vibrator;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MyDialog.ResultsListener {
+
+
 
     int counter;
     ImageView egg;
     TextView tv;
     SharedPreferences sp;
-
+    VideoView explosion;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Vibrator vibrator = (Vibrator) this.getSystemService(Context.VIBRATOR_SERVICE);
-
+        this.explosion = (VideoView)findViewById(R.id.explosion);
         this.egg = (ImageView)findViewById(R.id.egg);
         this.tv = (TextView)findViewById(R.id.number);
+        this.btn = (Button)findViewById(R.id.button);
         sp = PreferenceManager.getDefaultSharedPreferences(this);
-        counter = sp.getInt("numOfClicks", 100000);
+        counter = sp.getInt("numOfClicks", 10000);
         tv.setText(Integer.toString(counter));
 
         final Animation animShake = AnimationUtils.loadAnimation(this, R.anim.shake);
+
+
+        String uriPath = "android.resource://com.example.madaim.ex12/"+ R.raw.explosion_10;
+        Uri uri = Uri.parse(uriPath);
+        explosion.setVideoURI(uri);
+
 
         egg.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -46,9 +61,34 @@ public class MainActivity extends AppCompatActivity {
                 tv.setText(Integer.toString(counter));
                 vibrator.vibrate(50);
                 egg.startAnimation(animShake);
+                if(counter<10000&&counter>=9500)
+                    egg.setImageResource(R.mipmap.egg);
+                if(counter<9500&&counter>=9000)
+                    egg.setImageResource(R.mipmap.egg1);
+                if(counter<9000&&counter>=8500)
+                    egg.setImageResource(R.mipmap.egg2);
+                if(counter<8500&&counter>=8000)
+                    egg.setImageResource(R.mipmap.egg3);
+                if(counter<8000&&counter>=7500)
+                    egg.setImageResource(R.mipmap.egg4);
+                if(counter<7500&&counter>=7000)
+                    egg.setImageResource(R.mipmap.egg5);
+                if(counter<7000&&counter>=6500)
+                    egg.setImageResource(R.mipmap.egg6);
 
             }
         });
+
+        btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                explosion.setSystemUiVisibility(View.VISIBLE);
+                explosion.requestFocus();
+                explosion.start();
+            }
+        });
+
 
     }
 
@@ -59,6 +99,8 @@ public class MainActivity extends AppCompatActivity {
         editor.commit();
         super.onPause();
     }
+
+
 
     public static class MyPreferences extends PreferenceFragment {
         @Override
@@ -74,8 +116,32 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.context, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_reset:
+                MyDialog.newInstance(MyDialog.RESET_DIALOG).show(getFragmentManager(), null);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-
+    @Override
+    public void onFinishedDialog(int requestCode, Object results) {
+        switch(requestCode) {
+            case MyDialog.RESET_DIALOG:
+                counter = 10000;
+                egg.setImageResource(R.mipmap.egg);
+                tv.setText(Integer.toString(counter));
+                break;
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/madaim/ex12/MyDialog.java
@@ -0,0 +1,77 @@
+package com.example.madaim.ex12;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Created by Kai on 3/31/2017.
+ */
+
+public class MyDialog extends DialogFragment {
+    private int requestCode;
+    public final static int RESET_DIALOG = 1;
+    private ResultsListener listener;
+
+    public static MyDialog newInstance(int requestCode) {
+        Bundle args = new Bundle();
+        MyDialog fragment = new MyDialog();
+        args.putInt("rc", requestCode);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    private AlertDialog.Builder buildExitDialog(){
+        return new AlertDialog.Builder(getActivity())
+                .setTitle("Reset")
+                .setMessage(R.string.reset)
+                .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        listener.onFinishedDialog(requestCode, "ok");
+
+                    }
+                })
+                .setNegativeButton("no", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dismiss();
+                    }
+                });
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        this.requestCode = getArguments().getInt("rc");
+        if (requestCode == RESET_DIALOG) {
+            return buildExitDialog().create();
+        }
+        else
+            return buildExitDialog().create();
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try{
+            this.listener = (ResultsListener)activity;
+        }
+        catch(ClassCastException e){
+            throw new ClassCastException("Hosting activity must implement the interface ResultsListener");
+        }
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        listener = null;
+    }
+
+    public interface ResultsListener{
+        public void onFinishedDialog(int requestCode, Object results);
+    }
+}
